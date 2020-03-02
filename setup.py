04ac63aa067cae4fa2dcc359fe8a4ef2c7cd0711
Shiva Prasad Adirala <shivaprasad.adirala@CaprusIT.local>
2020-03-02 23:45:37 +0530

    Increment release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
       name='robotframework-historic-listener',
-      version="0.1.3",
+      version="0.1.4",
       description='Listener to push robotframework execution results to MySQL',
       classifiers=[
           'Framework :: Robot Framework',
