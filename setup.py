ec0111c10ea6ea2c145081ba8e7c4ad15b71065b
Shiva Prasad Adirala <shivaprasad.adirala@CaprusIT.local>
2020-04-08 20:47:37 +0530

    twine upload dist/*

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
       name='robotframework-historic-listener',
-      version="0.1.7",
+      version="0.1.8",
       description='Listener to push robotframework execution results to MySQL',
       classifiers=[
           'Framework :: Robot Framework',
